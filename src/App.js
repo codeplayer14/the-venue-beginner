7027db0b0e941b62556ec0f4f3d50b9b48d91d54
codeplayer14 <padmanabh14@gmail.com>
2018-12-22 18:29:56 +0530

    Added feature component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./resources/styles.css";
 import Header from "./components/header_footer/Header";
-
+import Featured from "./components/featured/Featured";
 class App extends Component {
   render() {
     return (
@@ -10,6 +10,7 @@ class App extends Component {
         style={{ height: "1500px", background: "cornflowerblue" }}
       >
         <Header />
+        <Featured />
       </div>
     );
   }
