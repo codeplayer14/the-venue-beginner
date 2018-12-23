12a89df5fbc0cb11a7741758c4ea7402fa4f5ee6
codeplayer14 <padmanabh14@gmail.com>
2018-12-24 01:43:03 +0530

    Added highlights section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./resources/styles.css";
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured/Featured";
 import VenueNfo from "./components/venueNfo";
+import Highlights from "./components/Highlights";
 class App extends Component {
   render() {
     return (
@@ -13,6 +14,7 @@ class App extends Component {
         <Header />
         <Featured />
         <VenueNfo />
+        <Highlights />
       </div>
     );
   }
